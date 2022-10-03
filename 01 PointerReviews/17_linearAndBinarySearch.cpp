204b2c8ece88166c40e09d2f905fee8e41491181
amezaiku <amezaikupan@gmail.com>
2022-10-03 15:47:02 +0700

    Fix Sentineal Linear Search

--- 01 PointerReviews/17_linearAndBinarySearch.cpp
+++ 01 PointerReviews/17_linearAndBinarySearch.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
 //Problem 17 - 20
 int LinearSearch(int* a, int n, int key){
     int i = 0;
@@ -14,6 +18,9 @@ int SentinelLinearSearch(int* a, int n, int key){
 
     int i = 0;
     while (a[i] != key) i++;
+
+    //Return value of last to array
+    a[n - 1] = last;
 
     if(i < n - 1) return i;
     else if(last == key) return n-1;
@@ -44,3 +51,22 @@ int RecursiveBinanrySearch(int* a, int left, int right, int key){
     else return RecursiveBinanrySearch(a, mid + 1, right, key);
 }
 
+
+int main(){
+    int* a = new int[10];
+    for (int i = 0; i < 10; i++) a[i] = i;
+    
+
+    cout << "Find item exist in array: " << endl;
+    cout << "Linear Search: " << LinearSearch(a, 10, 4) << endl;
+    cout << "Sentiel Linear Search: " << SentinelLinearSearch(a, 10, 4) << endl;
+    cout << "Binanry Search: " << BinarySearch(a, 10, 4) << endl;
+    cout << "Recursive Binary Search: " << RecursiveBinanrySearch(a, 0, 9, 4) << endl;
+
+    cout << "Find item doesn't exist in array: " << endl;
+    cout << "Linear Search: " << LinearSearch(a, 10, 14) << endl;
+    cout << "Sentiel Linear Search: " << SentinelLinearSearch(a, 10, 14) << endl;
+    cout << "Binanry Search: " << BinarySearch(a, 10, 14) << endl;
+    cout << "Recursive Binary Search: " << RecursiveBinanrySearch(a, 0, 9, 14) << endl;
+
+}
